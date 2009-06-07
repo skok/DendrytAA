a71a6b9e84e3992557f7677c209db6cabc92ca51
skok <michal.radziwon@gmail.com>
2009-06-07 22:21:46 +0200

    .

--- src/com/dendrytdev/org/client/Employee/CommentServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.dendrytdev.org.client.employee;
-
-import com.dendrytdev.org.client.bean.Function;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface CommentServiceAsync {
-	void addComment(Long problemId,Function function,Boolean direction, String content, AsyncCallback<Object> asyncCallback);
-}
